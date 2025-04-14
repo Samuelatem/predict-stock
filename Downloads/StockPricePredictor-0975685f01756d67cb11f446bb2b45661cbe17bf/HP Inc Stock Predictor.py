5388708d8098c64a06e684bb4215267a617fa0f6
SAMUEL ATEM <love@Samuels-MacBook-Pro.local>
2025-04-14 12:28:45 +0100

    Added all project files for Apple Stock Predictor

---
+++ Downloads/StockPricePredictor-0975685f01756d67cb11f446bb2b45661cbe17bf/HP Inc Stock Predictor.py
@@ -0,0 +1,109 @@
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+import yfinance as yf   
+
+# Fetch data using yfinance
+df = yf.download('AAPL', start='2020-01-01', end='2025-12-28')
+
+new_df = df.filter(['Close'])
+
+#creating train and test datasets
+dataset = new_df.values
+#Get /Compute the number of rows to train the model on
+training_data_len = math.ceil( len(dataset) *.8) 
+
+#x_train and y_train
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+#Create the scaled training data set 
+train_data = scaled_data[0:training_data_len  , : ]
+
+#Split the data into x_train and y_train data sets
+x_train, y_train = [], []
+
+for i in range(60,len(train_data)):
+    x_train.append(train_data[i-60:i,0])
+    y_train.append(train_data[i,0])
+    
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+#Reshape the data into the shape accepted by the LSTM
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#LSTM network with 2 layers of LSTM (50 neurons each) & 2 Dense layers (25 in one, 1 in the other)
+model = Sequential()
+model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+model.add(LSTM(units = 50, return_sequences = False))
+model.add(Dense(units = 25))
+model.add(Dense(units = 1))
+
+#compiling the model
+model.compile(optimizer = "adam", loss = "mean_squared_error")
+
+#Training the model
+model.fit(x_train, y_train, batch_size = 1, epochs = 2)
+
+#creating test dataset
+#Test data set
+test_data = scaled_data[training_data_len - 60: , : ]
+#Create the x_test and y_test data sets
+x_test = []
+y_test =  dataset[training_data_len : , : ]  
+for i in range(60,len(test_data)):
+    x_test.append(test_data[i-60:i,0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+#Getting the models predicted price values
+closing_price = model.predict(x_test)
+closing_price = scaler.inverse_transform(closing_price)
+
+#rmse(root mean sqaure error)
+rms = np.sqrt(np.mean(np.power(y_test - closing_price, 2)))
+print(rms)
+
+#plotting the graph
+train = new_df[:training_data_len]
+valid = new_df[training_data_len:]
+valid['Prediction'] = closing_price
+
+plt.plot(train["Close"])
+plt.plot(valid[["Close", "Prediction"]])
+
+
+#predicting future values
+
+#Get teh last 60 day closing price 
+last_60_days = new_df[-60:].values
+#Scale the data to be values between 0 and 1
+last_60_days_scaled = scaler.transform(last_60_days)
+#Create an empty list
+X_test = []
+#Append teh past 60 days
+X_test.append(last_60_days_scaled)
+#Convert the X_test data set to a numpy array
+X_test = np.array(X_test)
+#Reshape the data
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#Get the predicted scaled price
+pred_price = model.predict(X_test)
+price = pred_price
+
+#undo the scaling 
+pred_price = scaler.inverse_transform(pred_price)
+print(pred_price)
+
+for i in range(1, 10):
+    X_test[0][0 : 59] = X_test[0][1 : 60]
+    X_test[0][59] = price
+    pred_price = model.predict(X_test)
+    price = pred_price
+    pred_price = scaler.inverse_transform(pred_price)
+    print(pred_price)
---
+++ Downloads/StockPricePredictor-0975685f01756d67cb11f446bb2b45661cbe17bf/README.md
@@ -0,0 +1,3 @@
+# StockPricePredictor
+
+This is a Machine Learning model developed to predict the future price of the stocks of a company all rights are reseved to samuel ATEM.
